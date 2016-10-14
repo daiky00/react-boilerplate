08ab56209dd9c7febd45aa4c9c2f25762956357c
Francisco Pena <project.01@hotmail.com>
2016-10-14 00:29:50 -0400

    added not found page plus styles and other stuff

--- src/containers/NotFound/NotFound.js
+++ src/containers/NotFound/NotFound.js
@@ -1,13 +1,30 @@
 import React, { Component } from 'react';
-import confused from './confused.svg'
-export default class NotFound extends Component {
+import {Grid, Row, Column} from 'react-cellblock';
+import confused from './confused.svg';
+import CSSModules from 'react-css-modules';
+import styles from './NotFound.css'
 
+class NotFound extends Component {
   render() {
     return (
-      <section>
-      <img src={confused} width="200px" height="200px"/>
-        <h1>404 NotFound</h1>
-        </section>
+      <section styleName="not-found">
+        <Grid>
+          <Row>
+            <Column offset="2/12" width="8/12">
+              <img src={confused} width="200px" height="200px"/>
+              <h1>404 NotFound</h1>
+              <h3>Opps Something went wrong please go to our homepage</h3>
+              <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor.
+              Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.
+              Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim.
+              Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut,
+              </p>
+            </Column>
+          </Row>
+        </Grid>
+      </section>
     );
   }
 };
+
+export default CSSModules(NotFound, styles);
